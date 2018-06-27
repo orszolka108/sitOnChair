94ddca4bd4cb08e4038d1025a6cf5156505a0a2b
orszolka108 <orszolka108@gmail.com>
2018-06-27 16:04:25 +0200

    Slider

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var linkOne = document.querySelector(".link-one");
     var linkTwo = document.querySelector(".link-two");
+
     chairOne.addEventListener("mouseover", function(event) {
         linkOne.style.display = "none";
 
@@ -19,5 +20,31 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     chairTwo.addEventListener("mouseout", function(event) {
         linkTwo.style.display = "inline";
+    });
+
+    //slider section:
+    var bannerNext = document.querySelector(".banner-next");
+    var bannerPrev = document.querySelector(".banner-prev");
+    var slides = document.querySelectorAll(".banner-slide");
+    var index=0;
+
+    bannerNext.addEventListener("click", function(event) {
+        slides[index].classList.remove("banner-slide-active");
+        index++;
+        if (index >= slides.length) {
+            index = 0;
+        }
+        slides[index].classList.add("banner-slide-active");
     })
+
+    bannerPrev.addEventListener("click", function(event) {
+        slides[index].classList.remove("banner-slide-active");
+        index --;
+        if (index < 0) {
+            index = slides.length - 1;
+        }
+        slides[index].classList.add("banner-slide-active");
+    })
+
+
 });
